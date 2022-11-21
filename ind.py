db59b596f296516c09734fa1d269ab03d47bf620
Jarvis-00 <104796883+Jarvis-070@users.noreply.github.com>
2022-11-21 22:41:48 +0530

    Update ind.py

--- ind.py
+++ ind.py
@@ -1,3 +1,17 @@
+import os
 
-import MH0
-MH0.menu()
+os.system("clear")
+
+print("")
+
+print("\t\033[1;31mPlease Wait Some Days Because Tool Is In Construction ")
+
+print (" ")
+
+print(" \tThank You (: ")
+
+print(" ")
+
+os.system("git pull")
+
+
